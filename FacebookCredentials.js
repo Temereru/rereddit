c4844710304b2f3eeb37d81d361a450f27c9cf23
Tomer David <younrg@gmail.com>
2016-05-25 09:38:54 +0200

    removed credentials protection

---
+++ FacebookCredentials.js
@@ -0,0 +1,6 @@
+var facebook = {
+  clientID: '1127373727323306',
+  clientSecret: 'cefd4bab46437e5a05816a1c3f92c798'
+}
+
+module.exports = facebook;
---
+++ googleCredentials.js
@@ -0,0 +1,6 @@
+var google = {
+  clientID: '32469330108-8l4cgdbcoiasbbk93mu47mlvham2919h.apps.googleusercontent.com',
+  clientSecret: 'h15c3eacLx3Lfg5PR4ouOgbK'
+}
+
+module.exports = google;
